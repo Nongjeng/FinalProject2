e9ecdb20a0230e6db0bd15a107f9409d13909b3f
Jeng GG <65309010007@tatc.ac.th>
2023-08-29 11:17:36 +0700

    cc

--- Controllers/conncet.php
+++ Controllers/conncet.php
@@ -14,5 +14,5 @@ $connect = mysqli_connect($host, $username, $password,$dbname);
 if ($connect->connect_error) {
     die("การเชื่อมต่อล้มเหลว: " . $connect->connect_error);
 }
-
+mysqli_set_charset($connect, "utf8");
 ?>
