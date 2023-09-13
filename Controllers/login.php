eb7df8cb94ba5cb479187e632207a8a3c0dfea60
Jeng GG <65309010007@tatc.ac.th>
2023-09-13 23:37:09 +0700

    login session lost

--- Controllers/login.php
+++ Controllers/login.php
@@ -2,15 +2,14 @@
 if(isset($_POST['login'])){
     $STD_ID  = $_POST["STD_ID"];
     $Password = $_POST["Password"];
+    $_SESSION['STD_ID'] = $STD_ID;
 
     $sql = "SELECT * FROM std WHERE STD_ID ='$STD_ID' AND Password='$Password'";
     $sql_q = mysqli_query($connect,$sql);
     if (!$sql_q) {
         echo "รหัสผ่านหรือชื่อผู้ใช้งานไม่ถูกต้อง";
     }else{
-        $_SESSION['STD_ID'] =  $STD_ID;
         header("Location: ?page=home");
         }
     }
-
 ?>
