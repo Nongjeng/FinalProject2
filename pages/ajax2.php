63678ffcff337d54da9f2a4ab176b11f8be2945c
LnwThaiz <tonnamnattapat@gmail.com>
2023-09-28 16:21:01 +0700

    fix bsc

---
+++ pages/ajax2.php
@@ -0,0 +1,18 @@
+<?php
+include "./Controllers/conncet.php";
+include "./Controllers/dateThai.php";
+
+if (isset($_POST['leave_id'])) {
+    $leave_id = $_POST['leave_id'];
+
+    // ทำการอัปเดตฐานข้อมูล
+    $update_status = "UPDATE leaves SET leave_status_id = 'LS02' WHERE leave_id = '$leave_id'";
+    $query = mysqli_query($connect, $update_status);
+
+    if ($query) {
+        echo 'success';
+    } else {
+        echo 'error';
+    }
+}
+?>
